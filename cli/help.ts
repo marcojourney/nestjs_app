76dd1504dd8904630ad67b5bc8bb0ecbf5501b70
Marco <mornsophannamis@gmail.com>
2023-10-03 22:00:50 +0700

    some modification

--- cli/help.ts
+++ cli/help.ts
@@ -1,7 +1,17 @@
-import * as yargs from 'yargs';
+// import * as yargs from 'yargs';
 
-yargs
-  .usage('Usage: node generate -n <entityName> -d <outputDirectory>')
-  .demandOption(['n', 'd'], 'Please provide both -n and -d options')
-  .alias('n', 'entityName')
-  .alias('d', 'outputDirectory').argv;
+// yargs
+//   .usage('Usage: node generate -n <entityName> -d <outputDirectory>')
+//   .demandOption(['n', 'd'], 'Please provide both -n and -d options')
+//   .alias('n', 'entityName')
+//   .alias('d', 'outputDirectory').argv;
+
+export function capitalizeString(input: string): string {
+  // Check if the input string is not empty
+  if (input.length === 0) {
+    return input; // Return the input string as is
+  }
+
+  // Capitalize the first letter and concatenate the rest of the string
+  return input.charAt(0).toUpperCase() + input.slice(1);
+}
--- cli/module.cli.ts
+++ cli/module.cli.ts
@@ -1,23 +1,78 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as yargs from 'yargs';
+import { capitalizeString } from './help';
+// import { hideBin } from 'yargs/helpers';
+
+// Import your entity and service creation functions here
+// import { createEntity } from './entity.cli';
 
 const argv = yargs
-  .usage('Usage: node generate -n <moduleName> -d <outputDirectory>')
-  .demandOption(['n', 'd'], 'Please provide both -n and -d options')
-  .alias('n', 'moduleName')
-  .alias('d', 'outputDirectory').argv;
+  .usage('Usage: node generate -n <moduleName>')
+  .demandOption(['n'], 'Please provide both -n options')
+  .alias('n', 'moduleName').argv;
 
-const BASE_PATH = 'src';
 const moduleName = argv['moduleName'];
-const outputDirectory = `${BASE_PATH}/${argv['outputDirectory']}`;
 
-const templatePath = path.join(__dirname, 'templates/entity.template.txt');
-const templateContent = fs.readFileSync(templatePath, 'utf-8');
+const configPath = path.join(`${__dirname}/..`, 'config.cli.json');
 
-const entityContent = templateContent.replace(/{name}/g, moduleName);
+fs.readFile(configPath, { encoding: 'utf8' }, (error, data) => {
+  if (error) {
+    console.error(`Error reading configuration file: ${error}`);
+  }
 
-const outputPath = path.join(outputDirectory, `${moduleName}.ts`);
-fs.writeFileSync(outputPath, entityContent);
+  try {
+    const config = JSON.parse(data);
+    const templatePath = 'templates';
+    const basePath = config.basePath;
+    const modulePath = `${basePath}/${moduleName}`;
+    const moduleDirectories = config?.directories || [];
 
-console.log(`Module ${moduleName} created at ${outputPath}`);
+    //check and create base path of module
+    if (!fs.existsSync(basePath)) {
+      fs.mkdirSync(basePath);
+    }
+
+    //create all nested directory in module
+    moduleDirectories.forEach((directory) => {
+      //directory inside module such as common, service, repository etc.
+      const subDirOfModule = `${modulePath}/${directory}`;
+      if (!fs.existsSync(subDirOfModule)) {
+        fs.mkdirSync(subDirOfModule, { recursive: true });
+
+        // Absolute path link to template file of cli
+        const templateName = directory;
+        const templateFile = `${__dirname}/${templatePath}/${templateName}.template.txt`;
+
+        if (fs.existsSync(templateFile)) {
+          const content = fs.readFileSync(templateFile, 'utf-8');
+
+          const className = moduleName;
+          const serviceContent = content.replace(/{name}/g, className);
+
+          const typeOfClass = subDirOfModule;
+          const fileName = `${capitalizeString(className)}${capitalizeString(
+            typeOfClass,
+          )}.ts`;
+          const pathFile = `${modulePath}/${fileName}`;
+          console.log('dddd:', pathFile);
+          // fs.writeFileSync(pathFile, serviceContent);
+        }
+      }
+    });
+  } catch (parseError) {
+    console.error(`Error parsing configuration file: ${parseError}`);
+  }
+});
+
+// const outputDirectory = `${BASE_PATH}/${argv['outputDirectory']}`;
+
+// const templatePath = path.join(__dirname, 'templates/entity.template.txt');
+// const templateContent = fs.readFileSync(templatePath, 'utf-8');
+
+// const entityContent = templateContent.replace(/{name}/g, moduleName);
+
+// const outputPath = path.join(outputDirectory, `${moduleName}.ts`);
+// fs.writeFileSync(outputPath, entityContent);
+
+// console.log(`Module ${moduleName} created at ${outputPath}`);
